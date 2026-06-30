b32828292c787110296860b2c8052e8a93e26982
Fernanda Lech <fernanda62545816@alunos.sc.senac.br>
2026-06-30 20:24:29 -0300

    Cadastrar novos alunos

--- sistema_pi/src/Alunos/NovosAlunos.java
+++ sistema_pi/src/Alunos/NovosAlunos.java
@@ -1,9 +1,9 @@
 package Alunos;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -33,7 +33,7 @@ public class NovosAlunos extends javax.swing.JFrame {
         btnVoltar = new javax.swing.JButton();
         txtNome = new javax.swing.JTextField();
         txtMatricula = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        cbTurma = new javax.swing.JComboBox<>();
         lblTitulo = new javax.swing.JLabel();
         btnCadastrar1 = new javax.swing.JButton();
 
@@ -53,7 +53,12 @@ public class NovosAlunos extends javax.swing.JFrame {
             }
         });
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbTurma.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione...", "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbTurma.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbTurmaActionPerformed(evt);
+            }
+        });
 
         lblTitulo.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
         lblTitulo.setText("NOVO ALUNO");
@@ -83,7 +88,7 @@ public class NovosAlunos extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lblTurma)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(cbTurma, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -110,7 +115,7 @@ public class NovosAlunos extends javax.swing.JFrame {
                     .addComponent(txtMatricula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cbTurma, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblTurma))
                 .addGap(18, 18, 18)
                 .addComponent(btnCadastrar1)
@@ -123,12 +128,32 @@ public class NovosAlunos extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCadastrar1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrar1ActionPerformed
-        // TODO add your handling code here:
+        try{
+            Connection conn = conexao.conexao.conectar();
+
+            String sql="INSERT INTO aluno(nome,matricula,turma) VALUES (?,?,?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1,txtNome.getText());
+            stmt.setInt(2,Integer.parseInt(txtMatricula.getText()));
+            stmt.setString(3, cbTurma.getSelectedItem().toString());
+
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "Salvo!");
+            stmt.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
     }//GEN-LAST:event_btnCadastrar1ActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
        this.dispose();
     }//GEN-LAST:event_btnVoltarActionPerformed
+
+    private void cbTurmaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbTurmaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbTurmaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -169,7 +194,7 @@ public class NovosAlunos extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCadastrar1;
     private javax.swing.JButton btnVoltar;
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> cbTurma;
     private javax.swing.JLabel lblMatricula;
     private javax.swing.JLabel lblNome;
     private javax.swing.JLabel lblTitulo;
